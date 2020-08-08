83352c0fbb5b44ac5e5afb9ef86d48c0c2fe536d
Luís Eduardo <eduardovansilva@gmail.com>
2020-08-08 10:56:26 -0300

    Coding stormGlass Interfaces and normilize method

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,6 +1,6 @@
 import { StormGlass } from '@src/clients/stormGlass';
 import axios from 'axios';
-import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json'
+import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json';
 import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
 
 jest.mock('axios');
@@ -9,9 +9,11 @@ describe('StromGlass client', () => {
   it('Should return the normalized forecast from the StormGlass service', async () => {
     const lat = -33.792726;
     const lng = 151.289824;
-    axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture)
+    axios.get = jest
+      .fn()
+      .mockResolvedValue({ data: stormGlassWeather3HoursFixture });
     const stormGlass = new StormGlass(axios);
     const response = await stormGlass.fetchPoints(lat, lng);
-    expect(response).toEqual(stormGlassNormalized3HoursFixture)
-  })
-})
+    expect(response).toEqual(stormGlassNormalized3HoursFixture);
+  });
+});
