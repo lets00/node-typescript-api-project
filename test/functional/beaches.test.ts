b8a2e102e84b6f51a6821b93db769ea37fc6e0c8
Luís Eduardo <eduardovansilva@gmail.com>
2020-08-19 21:23:55 -0300

    Integrate Beach and Forecast and mocking api with nock

--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -1,5 +1,4 @@
-import { BeachPosition } from '@src/services/forecast';
-import { Beach } from '@src/models/beach';
+import { Beach, BeachPosition } from '@src/models/beach';
 
 describe('Beaches funcional tests', () => {
   beforeAll(async () => await Beach.deleteMany({}));
